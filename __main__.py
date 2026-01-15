bde6129a0e3c771d6ccba645a6786fd7f999fb25
IsakEliH <isakelihauksson@gmail.com>
2026-01-15 10:23:51 +0000

    main start and map

---
+++ __main__.py
@@ -0,0 +1,13 @@
+from map import Map
+from player import Player
+
+
+def main():
+    while True:
+        mapper = Map(posistion, lever_tiles, tiles, victory_tiles)
+        print("SHIT")
+        break
+
+
+if __name__ == "__main__":
+    main()
