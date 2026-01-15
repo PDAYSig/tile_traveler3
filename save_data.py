d7421c457c69e4de2a8c93b922fb7e571d5ace7e
PDAYSig <patrikdagur@gmail.com>
2026-01-15 10:49:02 +0000

    save state functionality

---
+++ save_data.py
@@ -0,0 +1,20 @@
+class SaveData:
+    SAVE_FILE = "save_file.txt"
+    @staticmethod
+    def save_game(
+            map : dict[tuple, str],
+            player_tile : tuple[int, int],
+            winning_tile : tuple[int, int],
+            coin_tally : int,
+            unflipped_levers : list[tuple]
+        ) -> None:
+        save_state = open(SaveData.SAVE_FILE, "w+", encoding="utf-8")
+        for tile in map:
+            save_state.write(f"{tile} {map[tile]}\n")
+        save_state.write(f"{player_tile}\n")
+        save_state.write(f"{winning_tile}\n")
+
+
+    @staticmethod
+    def load_game():
+        pass
