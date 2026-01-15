ee9f44317177fb159e7d617f4a0cf1caf4cdc4eb
IsakEliH <isakelihauksson@gmail.com>
2026-01-15 10:43:43 +0000

    Added docstring to Player

--- __main__.py
+++ __main__.py
@@ -1,10 +1,20 @@
+""" The Game Loop """
+
 from map import Map
 from player import Player
 
 
 def main():
+    """ Run The Game Loop """
+    
+    # Initializes the player and map when starting
+
+    # Create Player() 
+    # Create Map(position, lever_tiles, tiles, victory_tiles)
+
+
+    # Run the game loop for player
     while True:
-        #mapper = Map(posistion, lever_tiles, tiles, victory_tiles)
         print("SHIT")
         break
 
