054868efc9a2a6ddb6cc826eda59b178f34f49a7
JBT13 <jeremiahborjas@gmail.com>
2026-01-15 10:09:14 +0000

    hello world

--- func.py
+++ func.py
@@ -1,9 +1,22 @@
+from random import randint
+
 def get_coin():
+    amount_of_coins = randint(1,10)
     ### Randomize later on
-    return 1
+    return amount_of_coins
+
+def game_over():
+    while True:
+        print("GAME OVER!!!")
 
 def lever_pull():
     ### Randomize later on
+    chance = randint(1,100)
+    death = 67
+
+    if death == chance:
+        return game_over()
+
     return get_coin()
 
 def move():
@@ -15,3 +28,4 @@ def available_moves():
 def current_location():
     # 
     pass
+
